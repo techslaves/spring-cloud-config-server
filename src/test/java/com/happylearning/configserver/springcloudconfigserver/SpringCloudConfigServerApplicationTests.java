b60c149d6abf4081385b0e879314dc35aff4105a
Utsav Garg <er.utsavgarg@gmail.com>
2021-07-01 19:26:27 +0530

    Initial Commit

---
+++ src/test/java/com/happylearning/configserver/springcloudconfigserver/SpringCloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.happylearning.configserver.springcloudconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
